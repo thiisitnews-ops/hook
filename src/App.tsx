61d1131c874ac87fbf1e5652162d2173da69a0a6
thiisitnews-ops <thiisitnews@gmail.com>
2025-11-11 01:01:10 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🚀 Hook AI App is Live!</h1>
+      <p>If you see this, your Vercel deployment works 🎉</p>
+    </div>
+  )
+}
+
+export default App
